dc3a52d3fd0d049ec61db4f54714b992b842b291
Buse <busesimsekk29@gmail.com>
2026-05-24 13:59:20 +0300

    Initial commit: ALS Hastalik Takip projesi bos mimari ile baslatildi

---
+++ src/main/java/com/buse/alstracker/AlsDiseaseTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.buse.alstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlsDiseaseTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlsDiseaseTrackerApplication.class, args);
+    }
+
+}
