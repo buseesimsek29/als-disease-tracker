02626d588aac09a4a70017b4243c294d154a4517
Buse <busesimsekk29@gmail.com>
2026-05-24 15:23:15 +0300

    Feature: Veritabanı guvenligini saglamak ve arayuza sadece ihtiyac duyulan semptom verilerini tasımak icin DTO katmanı  olusturuldu

---
+++ src/main/java/dto/SymptomLogDTO.java
@@ -0,0 +1,18 @@
+package dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SymptomLogDTO {
+
+    private Long id;
+    private String symptomName;
+    private Integer severity;
+    private String notes;
+    private LocalDateTime logDate;
+}
