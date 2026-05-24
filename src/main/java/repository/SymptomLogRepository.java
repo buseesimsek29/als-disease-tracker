294e8b829e44e626aff4f85c90601d616b134187
Buse <busesimsekk29@gmail.com>
2026-05-24 15:03:56 +0300

    Feature: Entity ve Repository katmanlari hatasız tamamlandı

---
+++ src/main/java/repository/SymptomLogRepository.java
@@ -0,0 +1,14 @@
+package repository;
+
+import entity.SymptomLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SymptomLogRepository extends JpaRepository<SymptomLog, Long> {
+
+    // Semptom adına göre filtreleme yapacak olan Derived Query
+    List<SymptomLog> findBySymptomNameContainingIgnoreCase(String symptomName);
+}
